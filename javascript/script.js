cff77d7db4f9a1380caf568435ab2e99b01931ab
Guillaume PRIGENT <guillaume.prigent92@gmail.com>
2019-12-24 13:34:15 +0100

    cards improvements

---
+++ javascript/script.js
@@ -0,0 +1,29 @@
+/* Include navbar on all pages */
+function includeHTML(includedFile) {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute(includedFile);
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          elmnt.removeAttribute(includedFile);
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+}
+
+
